d3470ef969c93771efe866afd562e01691d05ef9
GitHub Action <action@github.com>
2021-10-07 01:35:02 +0000

    Template cleanup

--- src/main/kotlin/com/github/younghokimseoul/ytemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/younghokimseoul/ytemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.younghokimseoul.ytemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.younghokimseoul.ytemplate.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/younghokimseoul/ytemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/younghokimseoul/ytemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.younghokimseoul.ytemplate.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.younghokimseoul.ytemplate.MyBundle
 
 class MyProjectService(project: Project) {
 
